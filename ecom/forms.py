373be558fa62de97603fcc4528246885098fac01
Deepikakcpoudel <123675705+Deepikakcpoudel@users.noreply.github.com>
2023-04-27 00:59:00 -0500

    Added customer address page Worked on payment UI Created download invoice feature

--- ecom/forms.py
+++ ecom/forms.py
@@ -41,3 +41,9 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = models.Orders
         fields = ['status']
+
+# for contact us page
+class ContactusForm(forms.Form):
+    Name = forms.CharField(max_length=30)
+    Email = forms.EmailField()
+    Message = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
